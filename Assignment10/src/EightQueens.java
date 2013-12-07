68edbda5139c553064664695065f37557a795d8d
LogicLion <thomashocking7@me.com>
2013-12-07 11:36:21 -0600

    Updated EightQueens

--- Assignment10/src/EightQueens.java
+++ Assignment10/src/EightQueens.java
@@ -1,16 +1,20 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.UIManager;
 
 
 public class EightQueens extends JFrame {
 	private Square[][] board = new Square[8][8];
 	private JPanel boardOfSquares = new JPanel(new GridLayout(8,8));
-	private JPanel ctrlAndInfoPanel = new JPanel();
+	private JPanel ctrlAndInfoPanel = new JPanel(new GridLayout(10,2));
 	private int queenCount;
 	private int nWins;
 	private int nLosses;
@@ -23,31 +27,107 @@ public class EightQueens extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		createContents();
 		setVisible(true);
+
+	}
+	
+	private class Listener implements ActionListener {
+		ActionListener pre;
+		
+		Listener( ActionListener al ) {
+			pre = al;
+		}	
+		
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			pre.actionPerformed(arg0);
+			markSquares();
+		}
 	}
 	
 	private void createContents(){
-
 		//create board.
 		for(int i = 0; i < board.length; i++){
 			board[i] = new Square[board.length];
 			for(int j = 0; j < board.length; j++){
-				boardOfSquares.add(board[i][j] = new Square());
+				board[i][j] = new Square();
+				ActionListener pre = board[i][j].getActionListeners()[0];
+				board[i][j].removeActionListener(pre);
+				board[i][j].addActionListener(new Listener(pre));
+				boardOfSquares.add(board[i][j]);	
 			}
 		}
-		ctrlAndInfoPanel.setPreferredSize(new Dimension(200,600));
+		ctrlAndInfoPanel.setPreferredSize(new Dimension(200, 600));
+		ctrlAndInfoPanel.add(new JLabel("Controls", JLabel.CENTER));
+		ctrlAndInfoPanel.add(new JLabel("Show unsafe spaces?", JLabel.CENTER));
 		add(ctrlAndInfoPanel,BorderLayout.EAST);
 		add(boardOfSquares,BorderLayout.CENTER);
-	
-		
-		
-		
+
 	}
 	
 	public void markSquares(){
-		
+		System.out.println("Marking squares");
+		for(int i = 0; i < board.length; i++){
+			for(int j = 0; j < board.length; j++){
+				if(board[i][j].getState() == Square.QUEEN){
+					//System.out.println("Found a queen at: " + (i+1) + "," +  (j+1) );
+					markSquaresForOneQueen(i,j);
+				}
+			}
+		}
 	}
 	
 	public void markSquaresForOneQueen(int qRow, int qCol){
+	
+		int x = qRow;
+		int y = qCol;
+		
+		if ( x < y ) {
+			y -= x;
+			x = 0;
+		} else {
+			x -= y;
+			y = 0;
+		}
+		while ( x < board.length && y < board.length) {
+			System.out.println ( "X: " + x + ", Y: " + y);
+			board[x][y].markUnsafe();
+			x++;
+			y++;
+		}
+		
+		
+		y = qCol;
+		for(int i = 0; i < board.length; i++){
+			board[i][y].markUnsafe();
+		}
+		
+		x = qRow;
+		for(int i = 0; i < board.length; i++){
+			board[x][i].markUnsafe();
+		}
+		
+		x = qRow;
+		y = qCol;
+		
+		if ( x < board.length - y ) {
+			System.out.println ("X < Y" + "X:" + x + ",Y:" + y);
+			x += y;
+			y = 0;
+		} else {
+			System.out.println("Y <= X" + "X:" + x + ",Y:" + y);
+			y -= board.length - 1 - x;
+			x = board.length - 1;
+		}
+		
+		System.out.println("Starting /");
+		
+		while( 0 <= x && y < board.length ) {
+			System.out.println( "X: " + x + ", Y: " + y);
+			board[x][y].markUnsafe();
+			y++;
+			x--;
+		}
+		
 		
 	}
 	
--- Assignment10/src/Square.java
+++ Assignment10/src/Square.java
@@ -1,15 +1,52 @@
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 
 
 public class Square extends JButton {
 	
 	private int state;
+	public final static int QUEEN = 1;
+	public final static int DANGER = 2;
+	public final static int OPEN = 3;
 	
+	private static Image queen;
 	
 	public Square(){
 		super.setContentAreaFilled(false);
+		state = OPEN;
+		addActionListener(new Listener());
+	}
+
+	private static Image getQueen() {
+		if ( queen == null ) {
+			//load queen
+			BufferedImage ibuf;
+			try {
+				ibuf = ImageIO.read(new File("queen-icon.jpeg"));
+				queen = ibuf;
+			} catch (IOException e) {
+				System.out.println("Cannot find image.");
+			}
+		}
+		return queen;
+	}
+	
+	private class Listener implements ActionListener{
+
+		public void actionPerformed(ActionEvent arg0) {
+			if(state == OPEN) state = QUEEN;
+			
+		}
+		
 	}
 	
 	public int getState(){
@@ -17,17 +54,26 @@ public class Square extends JButton {
 	}
 	
 	public void markUnsafe(){	
-		
+		if ( state != QUEEN ) {
+			setBackground(Color.red);
+			this.state = DANGER;
+		}
 	}
 	
 	public void clear(){
-		
+		setBackground(Color.white);
+		this.state = OPEN;
 	}
 	
 	protected void paintComponent(Graphics g){
-		g.setColor(getBackground());
-		g.fillRect(0, 0, getWidth(), getHeight());
+		if ( state == QUEEN) {
+			g.drawImage(getQueen(), 0, 0, getWidth(), getHeight(), null);			
+		} else {
+			g.setColor(getBackground());
+			g.fillRect(0, 0, getWidth(), getHeight());
+		}
 		super.paintComponent(g);
 	}
+	
 }
 	
